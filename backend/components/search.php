7e740a21fc7c35bde298b6da642eef54d21b3fb3
dimweb <dmitrymail_k@mail.ru>
2020-12-22 20:57:20 +0400

    add search on 2 fields

--- backend/components/search.php
+++ backend/components/search.php
@@ -9,38 +9,6 @@ function clear_query($query)
   return $query;
 }
 
-// function searchOnPage($link, $query, $page_name)
-// {
-//   $text = file_get_contents($link);
-//   $text = preg_replace('/(<(style)\b[^>]*>).*?(<\/\2>)/is', "$1$3", $text);
-//   preg_match("/<body.*\/body>/s", $text, $html_body);
-
-//   $text = strip_tags($html_body[0]); // pure text from page
-//   $text = mb_strtolower(preg_replace('/\s+/', ' ', $text));
-
-//   $pos = strpos($text, $query);
-
-//   if ($pos === false) {
-//     return false; // query doesn't exist
-//   }
-
-//   $searched_query = "";
-
-//   if ($pos > 16)
-//     $searched_query = substr($text, ($pos - 16));
-//   else
-//     $searched_query = substr($text, $pos);
-
-//   // $searched_query = implode(" ", $searched_query);
-//   $searched_query = mb_strimwidth($searched_query, 1, 120, "...", mb_internal_encoding());
-//   $searched_query = str_replace($query, "<b>$query</b>", $searched_query);
-
-//   $result['content'] = $searched_query;
-//   $result['link'] = $link;
-//   $result['title'] = $page_name;
-//   return $result;
-// }
-
 require_once "../connect.php";
 $search_query = clear_query($_POST['search']);
 if(strlen($search_query)>0){
@@ -48,8 +16,8 @@ if(strlen($search_query)>0){
   $results = [];
   $products = [];
   try {
-    $products_query = $pdo->prepare("SELECT * FROM `products` WHERE title LIKE ?");
-    $products_query->execute(["%$search_query%"]);
+    $products_query = $pdo->prepare("SELECT * FROM `products` WHERE (title LIKE ? OR price LIKE ?)");
+    $products_query->execute(["%$search_query%", "%$search_query%"]);
     $products = $products_query->fetchAll();
   } catch(PDOException $e) {
     echo $e->getMessage();
